0c25915ac596b57dccc49b9a500d726501f14ec4
jasmo2 <jasmo2.com@gmail.com>
2015-10-31 20:39:52 -0500

    add Profile and My_file.rb

---
+++ lib/My_json.rb
@@ -0,0 +1,11 @@
+require 'syro'
+require 'json'
+require_relative './query_maker'
+class MyJson <  Syro::Deck
+  def default_headers
+    { 'Content-Type' => 'application/json' }
+  end
+  def my_response(obj)
+    res.write JSON.dump({'ans' => "this is the answer for #{obj[:placa_bus]}"}.to_json)
+  end
+end
