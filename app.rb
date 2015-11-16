2d700f9caa3ec41a53a00070d22535b5f3b7ec6f
jasmo2 <jasmo2.com@gmail.com>
2015-11-15 21:33:25 -0500

    change 'placa_bus' to 'bus_id'

--- app.rb
+++ app.rb
@@ -4,15 +4,15 @@ App = Syro.new(MyJson) {
   get{
     res.write "root"
   }
-  on("placa_bus") {
-    on(:placa_bus) {
+  on("bus_id") {
+    on(:bus_id) {
       on("fecha_i"){
         on(:fecha_i){
           on("fecha_f"){
             on(:fecha_f){
               get {
                 my_response(
-                    placa_bus: inbox[:placa_bus],
+                    bus_id: inbox[:bus_id],
                     fecha_i: Time.at(inbox[:fecha_i].to_i),
                     fecha_f: Time.at(inbox[:fecha_f].to_i),
                 )
